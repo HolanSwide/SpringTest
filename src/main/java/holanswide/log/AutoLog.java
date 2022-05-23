f6ff63901ec5219306c93bb5eb1966fb92f9cae3
holanswide <holanswide@yeah.net>
2022-05-23 20:35:05 +0800

    AOP实现 注解/配置文件

---
+++ src/main/java/holanswide/log/AutoLog.java
@@ -0,0 +1,10 @@
+package holanswide.log;
+
+/**
+ * @author ：holan
+ * @description：TODO
+ * @date ：2022/5/23 20:08
+ */
+
+public class AutoLog {
+}
---
+++ src/main/java/holanswide/log/SelfLog.java
@@ -0,0 +1,10 @@
+package holanswide.log;
+
+/**
+ * @author ：holan
+ * @description：TODO
+ * @date ：2022/5/23 19:46
+ */
+
+public class SelfLog {
+}
